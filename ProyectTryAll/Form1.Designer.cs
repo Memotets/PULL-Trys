fd8120869f031bd355adee3dd82a03e83368eb59
GuillermoBautista <guillermo.bg99@gmail.com>
2022-05-13 18:47:51 -0500

    First commit, window without borders and Top most setting on form, ventana sin fondo y top most setteado

---
+++ ProyectTryAll/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace ProyectTryAll
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PanelLogin = new System.Windows.Forms.Panel();
+            this.LabelLogin = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.PanelLogin.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PanelLogin
+            // 
+            this.PanelLogin.BackColor = System.Drawing.Color.Teal;
+            this.PanelLogin.Controls.Add(this.button1);
+            this.PanelLogin.Controls.Add(this.LabelLogin);
+            this.PanelLogin.Location = new System.Drawing.Point(98, 164);
+            this.PanelLogin.Name = "PanelLogin";
+            this.PanelLogin.Size = new System.Drawing.Size(490, 353);
+            this.PanelLogin.TabIndex = 5;
+            this.PanelLogin.Paint += new System.Windows.Forms.PaintEventHandler(this.PanelLogin_Paint);
+            // 
+            // LabelLogin
+            // 
+            this.LabelLogin.AutoSize = true;
+            this.LabelLogin.BackColor = System.Drawing.Color.LightSlateGray;
+            this.LabelLogin.Cursor = System.Windows.Forms.Cursors.Default;
+            this.LabelLogin.Font = new System.Drawing.Font("Georgia", 16.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelLogin.ForeColor = System.Drawing.Color.White;
+            this.LabelLogin.Location = new System.Drawing.Point(129, 33);
+            this.LabelLogin.Name = "LabelLogin";
+            this.LabelLogin.Size = new System.Drawing.Size(232, 32);
+            this.LabelLogin.TabIndex = 0;
+            this.LabelLogin.Text = "Prueba de sesion ";
+            this.LabelLogin.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Georgia", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(76, 258);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(318, 65);
+            this.button1.TabIndex = 1;
+            this.button1.TabStop = false;
+            this.button1.Text = "Cerrar ventana";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PowderBlue;
+            this.ClientSize = new System.Drawing.Size(782, 753);
+            this.ControlBox = false;
+            this.Controls.Add(this.PanelLogin);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.PanelLogin.ResumeLayout(false);
+            this.PanelLogin.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel PanelLogin;
+        private System.Windows.Forms.Label LabelLogin;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ProyectTryAll/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectTryAll
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PanelLogin_Paint(object sender, PaintEventArgs e)
+        {
+            // code for initializing the panel and setting 
+            // its size goes here
+
+            PanelLogin.Location = new Point(
+                this.ClientSize.Width / 2 - PanelLogin.Size.Width / 2,
+                this.ClientSize.Height / 2 - PanelLogin.Size.Height / 2);
+            PanelLogin.Anchor = AnchorStyles.None;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //si se desea cerrar una subventana usar System.Windows.Forms.Application.ExitThread() o this.exit();
+            System.Windows.Forms.Application.Exit();
+        }
+    }
+}
